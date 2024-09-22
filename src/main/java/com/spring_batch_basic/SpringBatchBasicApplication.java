124f4a260c9fedcbf70edb1dd41e36532d674f93
lootan_h <loosung@naver.com>
2024-09-22 13:08:25 +0900

    first initialize

---
+++ src/main/java/com/spring_batch_basic/SpringBatchBasicApplication.java
@@ -0,0 +1,12 @@
+package com.spring_batch_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SpringBatchBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_batch_basic/SpringBatchBasicApplicationTests.java
@@ -0,0 +1,11 @@
+package com.spring_batch_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SpringBatchBasicApplicationTests {
+
+	@Test void contextLoads() {
+	}
+
+}
